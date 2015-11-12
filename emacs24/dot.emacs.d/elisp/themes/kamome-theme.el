4bd688975a8c8335b5a95bd0a6f2ed7019ba2e90
kamome <kamome4911@gmail.com>
2015-11-12 20:12:43 +0900

    変更をマージ

--- emacs24/dot.emacs.d/elisp/themes/kamome-theme.el
+++ emacs24/dot.emacs.d/elisp/themes/kamome-theme.el
@@ -3,38 +3,47 @@
 
 (custom-theme-set-faces
  'kamome
- ;; 背景・文字・カーソル
- ;;'(cursor  ((t (:foreground "#cd0000"))))
+
+ ;; base
  '(default ((t (:foreground "#ffffff" :background "#000000"))))
- ;; 文字種
+
+ ;; text
  '(bold        ((t (:italic nil :bold nil :weight normal))))
  '(bold-italic ((t (:italic nil :bold nil :weight normal :slant normal))))
- ;; 選択範囲
+
+ ;; region
  '(region ((t (:background "#3a3a3a"))))
+
+ ;; mini-buffer
  '(minibuffer-prompt ((t (:foreground "#cd0000"))))
- ;; モードライン
+
+ ;; mode-line
  '(mode-line           ((t (:foreground "#ffffff" :background "#005faf" :box (:line-width 1 :color "#ffffff" :style released-button)))))
  '(mode-line-inactive  ((t (:foreground "#ffffff" :background "#7f7f7f" :box (:line-width 1 :color "#ffffff")))))
  '(mode-line-buffer-id ((t (:foreground nil :background nil))))
- ;; ハイライト
- '(highlight      ((t (:foreground "#000000" :background "#C4BE89"))))
+
+ ;; hightlight
  '(hl-line        ((t (:underline t))))
+ '(highlight      ((t (:foreground "#000000" :background "#C4BE89"))))
  '(lazy-highlight ((t (:foreground "#000000" :background "#C4BE89"))))
- ;; font lock
+
+ ;; font-lock
  '(font-lock-preprocessor-face  ((t (:foreground "#8700ff"))))
- '(font-lock-function-name-face ((t (:foreground "#af005f"))))
- '(font-lock-type-face          ((t (:foreground "#af875f"))))
- '(font-lock-variable-name-face ((t (:foreground "#00cdcd"))))
- '(font-lock-string-face        ((t (:foreground "#808080"))))
- '(font-lock-keyword-face       ((t (:foreground "#005fd7"))))
+ '(font-lock-function-name-face ((t (:foreground "#0000ff"))))
+ '(font-lock-type-face          ((t (:foreground "#ffff00"))))
+ '(font-lock-variable-name-face ((t (:foreground "#87afff"))))
+ '(font-lock-string-face        ((t (:foreground "#87005f"))))
+ '(font-lock-keyword-face       ((t (:foreground "#00afff"))))
  '(font-lock-builtin-face       ((t (:foreground "#0000ee"))))
- '(font-lock-constant-face      ((t (:foreground "#d75f00"))))
+ '(font-lock-constant-face      ((t (:foreground "#878700"))))
  '(font-lock-error-face         ((t (:foreground "#cd0000"))))
  '(font-lock-warning-face       ((t (:foreground "#cd0000"))))
- '(font-lock-comment-face       ((t (:foreground "#005f00"))))
+ '(font-lock-comment-face       ((t (:foreground "#008700"))))
+
  ;; ispell
  ;;'(flyspell-duplicate ((t (:bold nil))))
  ;;'(flyspell-incorrect ((t (:bold nil))))
+
  ;; rainbow-delimiters
  '(rainbow-delimiters-depth-1-face ((t (:foreground "#ffffff"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "#cd0000"))))
@@ -45,26 +54,13 @@
  '(rainbow-delimiters-depth-7-face ((t (:foreground "#00ffff"))))
  '(rainbow-delimiters-depth-8-face ((t (:foreground "#af00ff"))))
  '(rainbow-delimiters-depth-9-face ((t (:foreground "#00d700"))))
+
  ;; Paren
  '(show-paren-match-face ((t (:foreground "#1B1D1E" :background "#949494"))))
+
  ;; Popup
- '(popup-face   ((t (:foreground "#000000" :background "#ffffff"))))
- ;; org
- '(org-todo                ((t (:foreground "#cd0000" :italic nil :bold nil :weight normal :slant normal))))
- '(org-agenda-date-weekend ((t (:foreground "#949494" :italic nil :bold nil :weight normal :slant normal))))
- '(org-agenda-date-today   ((t (:foreground "#af00ff" :italic nil :bold nil :weight normal :slant normal :underline t))))
- '(org-date                ((t (:foreground "#949494"))))
- ;; Css
- '(css-selector ((t (:foreground "#0000ee"))))
- '(css-property ((t (:foreground "#ff0000"))))
- ;; web
- '(web-mode-html-tag-bracket-face          ((t (:foreground "#0000ee"))))
- '(web-mode-html-tag-face                  ((t (:foreground "#0000ee"))))
- '(web-mode-doctype-face                   ((t (:foreground "#cd0000"))))
- '(web-mode-html-attr-name-face            ((t (:foreground "#cd0000"))))
- '(web-mode-current-element-highlight-face ((t (:foreground "#cd0000"))))
- '(web-mode-param-name-face                ((t (:foreground "#000000"))))
- '(php-annotations-annotation-face         ((t (:foreground "#005f00"))))
+ '(popup-face ((t (:foreground "#000000" :background "#ffffff"))))
+
  ;; helm base
  '(helm-header           ((t (:foreground "#ffffff" :background "#ff8700"))))
  '(helm-candidate-number ((t (:foreground "#ffffff" :background "#005faf"))))
@@ -72,6 +68,7 @@
  '(helm-selection        ((t (:background "#cdcd00"))))
  '(helm-match            ((t (:foreground "#af0087"))))
  '(helm-grep-match       ((t (:foreground "#af0087"))))
+
  ;; helm filer
  '(helm-buffer-file                 ((t (:foreground "#ffffff"))))
  '(helm-buffer-directory            ((t (:foreground "#005fff"))))
@@ -80,15 +77,41 @@
  '(helm-ff-directory                ((t (:foreground "#005fff" :background nil))))
  '(helm-ff-dotted-directory         ((t (:foreground "#7f7f7f" :background nil))))
  '(helm-ff-dotted-symlink-directory ((t (:foreground "#7f7f7f" :background nil))))
+
  ;; helm m-x
  '(helm-M-x-key ((t (:foreground "#cd0000" :bold nil))))
+
  ;; helm-ls-git-ls
  '(helm-ls-git-modified-and-staged-face ((t (:foreground "#00cdcd" :bold nil))))
  '(helm-ls-git-modified-not-staged-face ((t (:foreground "#cd0000" :bold nil))))
  '(helm-ls-git-untracked-face           ((t (:foreground "#7f7f7f" :bold nil))))
+
+ ;; org
+ '(org-todo                ((t (:foreground "#cd0000" :italic nil :bold nil :weight normal :slant normal))))
+ '(org-agenda-date-weekend ((t (:foreground "#949494" :italic nil :bold nil :weight normal :slant normal))))
+ '(org-agenda-date-today   ((t (:foreground "#af00ff" :italic nil :bold nil :weight normal :slant normal :underline t))))
+ '(org-date                ((t (:foreground "#949494"))))
+
+ ;; Css
+ '(css-selector ((t (:foreground "#0000ee"))))
+ '(css-property ((t (:foreground "#ff0000"))))
+
+ ;; web
+ '(web-mode-html-tag-bracket-face          ((t (:foreground "#0000ee"))))
+ '(web-mode-html-tag-face                  ((t (:foreground "#0000ee"))))
+ '(web-mode-doctype-face                   ((t (:foreground "#cd0000"))))
+ '(web-mode-html-attr-name-face            ((t (:foreground "#cd0000"))))
+ '(web-mode-current-element-highlight-face ((t (:foreground "#cd0000"))))
+ '(web-mode-param-name-face                ((t (:foreground "#000000"))))
+ '(php-annotations-annotation-face         ((t (:foreground "#005f00"))))
+
+ ;; php
+ '(php-annotations-annotation-face ((t (:foreground "#008700"))))
  )
-;; ###autoload
+
+;; autoload
 (when load-file-name
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
+
 (provide-theme 'kamome)
--- emacs24/dot.emacs.d/inits/30-other-init.el
+++ emacs24/dot.emacs.d/inits/30-other-init.el
@@ -1,1 +1,5 @@
 ;; その他の初期化処理
+
+;; cua
+(cua-mode t)
+(setq cua-enable-cua-keys nil)
